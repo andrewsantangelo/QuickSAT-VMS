94b8c67ccc94d624d0f870e1ff566b7d7d35ace6
andrewsantangelo <andrew_santangelo@mac.com>
2016-01-01 23:37:48 -0700

    New Year update
    Update to /bin

--- qs/bin/hangup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import radio_status
-import subprocess
-
-if __name__ == '__main__':
-	subprocess.call(['pkill', 'pppd'])
-	radio = radio_status.gsp1720(dtr_pin=None)
-	radio.hangup()
